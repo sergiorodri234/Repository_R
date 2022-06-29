18cfd7cec4d2e7ca319a12f3cf08f7ee554e0d09
LizethVillamil <lizethvillamil@usantotomas.edu.co>
2022-06-29 14:58:53 -0500

    Update archivo de proyecto clase.R

--- archivo de proyecto clase.R
+++ archivo de proyecto clase.R
@@ -1,49 +1,65 @@
-library('tidyverse')
-# Punto 1. Vectores y 
-### La principal diferencia existente entre un vector c() y una lista dentro de R 
-### viene dada por la restriccion presente de c() el cual al recibir tipos de datos distintos da problemas al no reconocer adecuadamente las diferencias de tipos
-### a diferencia de la lista que si permite distintos tipos de datos dentro de estas
+## SERGIO RODRIGUEZ Y LIZETH VILLAMIL
+## ANALISIS DE DATOS CON R
+
+
+##PUNTO 1
+# VECTORES Y LISTAS
+## A)
+# La principal diferencia existente entre un vector c() y una lista dentro de R 
+# es que un vector los elementos deben ser del mismo tipo, a diferencia de la lista 
+# que si permite distintos tipos de datos dentro de estas, 
+# en otras palabras la listas serian una correlación ordenada de cualquier objeto,
+# además de almacenar distintas estructuras.
+
+## B)
+
 lista_1 <- list(
   1:3,
   "a",
   c(TRUE,FALSE,TRUE),
   c(2.3,5.9)
 )
-# Se hace una asignacion  para ambos caso con el fin de visualizar el  type en el entorno de trabajo  
-lista_1_corcherte_normal <- lista_1[2] 
+# Se hace una asignacion  para ambos casoS con el fin de visualizar el  type en el entorno de trabajo  
+lista_1_corcherte_normal <- lista_1[2]
 lista_1[2]
-
-type(lista_1[1])
-lista_1_corchetes_dobles<- lista_1[[1]]
-l1[[4]]
+lista_1_corcherte_normal
 
 
-# con la asignacion anteorior, observamos que con  un solo corchete  se extrae un elemento tipo lista para la  posicion especificada ,que en 
-# y con corchetes dobles  se accede a los elementos de la posicion dada con el tipo de dato original
+lista_1_corchetes_dobles<- lista_1[[2]]
+lista_1[[2]]
+lista_1_corchetes_dobles
+
+# Con la asignacion anteorior, observamos que con  un solo corchete  se extrae un elemento tipo lista para la  posicion 
+# especificada ,que en y con corchetes dobles  se accede a los elementos de la posicion dada con el tipo de dato original
+# como un value.
+
+##PUNTO DOS 
 
 matriz <- matrix(rep(1:7, 4), ncol = 4, nrow = 7, byrow = TRUE)
-#
+
 # Solución:
+
 ## Literal a.
-matriz[,2]  # acceder a columna 2
-apply(matriz,2,)
+matriz[,2]  # Acceder a columna 2
 ## Literal b.
-matriz[3,]
+matriz[3,] # Acceder a la tercera fila únicamente.
 ## Literal c.
-matriz[3:7,]
+matriz[3:7,] # Acceder de la fila 3 a la fila 7.
 ## Literal d.
-matriz[1:2,2:4]
+matriz[1:2,2:4] # Acceder las columnas 2 a 4 de las filas 1 y 2.
 
-
+## PUNTO 3
 #Estructura
 numero <- 5
 
 # Solución:
-if ((numero %% 2==0)) {
+if (numero %% 2==0) {
   print(paste(numero,"Es par"))
-} else if ((numero %% 2!=0)) {
+} else if (numero %% 2!=0) {
   print(paste(numero,"Es impar"))
 }
+
+## PUNTO 4
 
 nombres <- c("Andrea", "Carlos", "Juan", "Carolina", "Fernando", "Laura")
 
@@ -51,7 +67,10 @@ nombres <- c("Andrea", "Carlos", "Juan", "Carolina", "Fernando", "Laura")
 for (nombre in nombres) {
   print(paste("Bienvenido",nombre))
 }
-#punto 5. Funciones
+
+## PUNTO 5. FUNCIONES
+
+# SOLUCION
 mi_funcion <<- function(a,b=1){
  return(a^b) 
 }
