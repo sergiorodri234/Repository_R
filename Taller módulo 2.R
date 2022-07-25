6b9cf624bbc27c85b61ced0a2b65fc3a5616ab08
sergiorodri234 <78710529+sergiorodri234@users.noreply.github.com>
2022-07-25 18:04:13 -0500

    Update Taller módulo 2.R

--- Taller módulo 2.R
+++ Taller módulo 2.R
@@ -8,7 +8,8 @@ all <- read_csv("https://raw.githubusercontent.com/sergiorodri234/raws/main/all_
                 col_types = cols(Average_price_2021 = col_double(), 
                                  Average_price__2020 = col_double()))
 
-colnames(all)
+colnames(all) ## se usa para ver los nombres de las columnas para cambiar sus nombre 
+
 # Cambio de nombres para facilitar acceso a las columnas/variables a utilizar -----
 
 all <- all %>% rename(ind = "...1" ,
@@ -30,45 +31,59 @@ all <- all %>% rename(ind = "...1" ,
 all %>% 
   glimpse() %>% 
   select(1:10)
+# se utiliza glimpse el metodo de dplyr para acceder a la estructura de las bases este es similar al metodo de r base str() se concatena con pipe para utilizar select
+# y establecerle la condicion que acceda para las columnas del 1 al 10
 
 # DataFrame con los missing values ----- 
 df_miss <- data.frame(miss_var_summary(all))
 
-df_miss
+df_miss # se usa para ver el resultado en la consola
+
 # Nueva columna con el  Cambio porcentual entre el año 2020 y 2021 ----
 all_ <- all %>% 
   select(avg_2020,avg_2021,country) %>% 
   mutate(cambio_pc = ((avg_2021 - avg_2020) / (avg_2020)*100)) %>% 
   arrange(desc(cambio_pc))
 
-
-
-all_
+all_   # se usa para ver el resultado en la consola
+# A continuacion se ven los 10 paises en los que mas se incremento el precio del internet para el periodo entre el 2020 y 2021 y la variacion correspondiente. 
+# 1.Saint Helena, Ascension and Tristan da Cunha con una variacion del 769%.
+# 2. Malawi con una variacion del 664%.
+# 3.São Tomé and Príncipe con una variacion del 430%.
+# 4. Mauritius con una variacion del 386%.
+# 5. Spain con una variacion del 223%.     
+# 6. French Polynesia con una variacion del 211%.
+# 7. Eswatini con una variacion del 154%.
+# 8. Greece con una variacion del 146%.
+# 9. Gibraltar con una variacion del 138%.
+# 10. Trinidad and Tobago con una variacion del 120%.
 # Velocidad promedio  de internet en megabits respecto a los continentes de la base -----
+
 all %>% 
   select(continental,avg) %>% 
   group_by(continental)  %>% 
   arrange((avg))
 # la region con el internet promedio mas lento corresponde a las cercanas al este ("Near East")
+
 paises_region_internet_promedio_mas_lento <- all %>%  
   filter(continental== "NEAR EAST" ) %>% 
   select(continental,country) 
 
-# Lo anterior corresponde  a un tibble donde se encuentra el listado de los paises pertenecientes a esta region
+# Lo anterior corresponde a un tibble donde se filtran los paises pertenecientes para esta region.
 
 #  grafico de dispersion  con las variables de velocidad promedio y porcentaje de usuarios de internet-----
 
 all<-all%>%
   mutate(all, porcentaje_usuarios = users/popula)
-##Lo que hicimos fue crear una función con mutate códico el cual nos facilitó
-#realizar la operación entre dos variables que en este caso ueron users y popula,
-#con el fin d encontrar el porcentaje de usuarios de internet.
+##Lo que hicimos fue crear una función con mutate código el cual nos facilitó
+#realizar la operación entre dos variables en este caso fueron users y popula,
+#con el fin de encontrar el porcentaje de usuarios de internet.
 
 
-## en este gráfico de dispersión podemos observar una gran dispersón entre los dados
-##pero aún asi podemos observar que la mayoría de los datos de usuarios de internet y 
-#y velocidad promedio, nos indica que hay gran cantidad de usuarios con una velocidad menor 
-#50 mbps.
+## En este gráfico de dispersión podemos observar una gran dispersión en los datos
+## denotamos que en la mayoría de casos los  de usuarios de internet y su 
+#velocidad promedio, es menor a 50 mbps.
+
 ggplot(all, aes(x=porcentaje_usuarios, y=avg))+
   geom_point(colour='orange', lwd=3)+
   theme_bw()+
